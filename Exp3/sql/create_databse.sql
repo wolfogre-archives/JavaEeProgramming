4cad82f9d74f738eb30ca5b842493da30838e414
Jason Song <wolfogre@outlook.com>
2016-04-24 12:58:40 +0800

    Update create_databse.sql

--- Exp3/sql/create_databse.sql
+++ Exp3/sql/create_databse.sql
@@ -11,18 +11,27 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- 导出 shopdb 的数据库结构
-DROP DATABASE IF EXISTS `shopdb`;
 CREATE DATABASE IF NOT EXISTS `shopdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `shopdb`;
 
 
+-- 导出  表 shopdb.user 结构
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 数据导出被取消选择。
+
+
 -- 导出  表 shopdb.producttable 结构
-DROP TABLE IF EXISTS `producttable`;
 CREATE TABLE IF NOT EXISTS `producttable` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `productCode` char(50) NOT NULL,
-  `productName` char(50) NOT NULL,
-  `productSource` char(50) NOT NULL,
+  `productCode` varchar(50) NOT NULL,
+  `productName` varchar(50) NOT NULL,
+  `productSource` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -30,7 +39,6 @@ CREATE TABLE IF NOT EXISTS `producttable` (
 
 
 -- 导出  表 shopdb.shoprecords 结构
-DROP TABLE IF EXISTS `shoprecords`;
 CREATE TABLE IF NOT EXISTS `shoprecords` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `userId` int(10) unsigned NOT NULL,
@@ -46,17 +54,6 @@ CREATE TABLE IF NOT EXISTS `shoprecords` (
 
 -- 数据导出被取消选择。
 
-
--- 导出  表 shopdb.user 结构
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` char(50) NOT NULL,
-  `password` char(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 数据导出被取消选择。
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
